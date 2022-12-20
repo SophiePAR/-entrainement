e8524f7cf9d464cd02f75eb05d863ad2068e4ff8
sophie Parisot <sophie.parisot.93@gmail.com>
2022-12-20 15:31:04 +0100

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_20_093449) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "flats", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "detail"
+    t.integer "price_per_night"
+    t.integer "number_of_guests"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
+  end
 
 end
